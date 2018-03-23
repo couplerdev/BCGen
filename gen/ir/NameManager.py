d794065eb3c01ba8d85ff1b9484b5b7e2cbefa2c
NeoGeon <987378754@qq.com>
2018-03-23 17:18:04 +0800

    add some function in ir.py and NameManager.py

--- gen/ir/NameManager.py
+++ gen/ir/NameManager.py
@@ -6,6 +6,59 @@
 #  reversion history:	
 #        2018,3,21        alex add the module
 #!/usr/bin/python
+from ir import AttrVect
+from ir import Model
+from ir import Mapper
+  
+
 
 class NameManager:
-	__
+    __slots__ = ['__attrVectDict','__mapperDict', '__gsMapDict', '_sMatDict',\
+                     '__modelNameDict']
+
+    def __init__(self):
+        self.__attrVectDict = {}
+        self.__mapperDict = {}
+        self.__gsMapDict = {}
+        self.__sMatDict = {}
+        self.__modelDict = {}
+	
+    
+    def CheckAttrVect(self, attrVect):
+        myName = ""
+        if attrVect.name != "":
+            if self.__attrVectDict.has_key(attrVect.name):
+                raise ValueError("user define attrVect name conflict!")
+            else:
+                self.__attrVectDict[attrVect.name]=1
+                myName = attrVect.name
+        else:
+            myName = attrVect.src + "2" + attrVect.dst + "_" + attrVect.grid + attrVect.pes
+            if self.__attrVectDict.has_key(myName):
+                # TODO may add some way to generate different name, but I think we should 
+                # TODO just raise error
+                raise ValueError("generate same attrVect name, please check your config")
+            else:
+                self.__attrVectDict[attrVect.name]=1
+        return myName
+		
+    def CheckModelName(self, Model):
+        myName = ""
+        if Model.name != "":
+            if self.__modelDict.has_key(model.name):
+                raise ValueError("user define model name conflict")
+            else:
+                self.__modelDict[Model.name]=1
+                myName = Model.name
+        else:
+            myName = model.name
+            if self.__modelDict.has_key(myName):
+                # TODO
+                # TODO
+                raise ValueError("generate same model name, please check your config")
+            else:
+                self.__modelDict[Model.name]=1
+        return myName
+
+    def CheckMapperName(self, Mapper):
+                
--- gen/ir/ir.py
+++ gen/ir/ir.py
@@ -3,35 +3,56 @@
 # intermediate representation
 
 class Subroutine:
-    __slots__=['sub_name','__argList']
-    def __init__(self):
-	self.sub_name=""
-	self.__argList = []
-
-    @sub_name.setter
-    def setName(self, name):
-	self.sub_name = name
+    __slots__=['__subroutineName','__argList']
+    def __init__(self, subroutineName="func",argList=[] ):
+	self.__subroutineName = subroutineName
+	self.__argList = argList
+        self.__lineCharacter = 50       
+ 
+    @property
+    def argList(self):
+        return self.__argList
+    @argList.setter
+    def argList(self, argListValue):
+        self.__argList = argListValue
 
     def toString(self):
-	pass
+	string = self.__subroutineName + "("
+        lenString = len(string)
+        lenSpace = lenString
+        for arg in self.__argList:
+            argStr = arg + ", "
+            string += argStr
+            lenString += len(argStr)
+            if lenString > self.__lineCharacter:
+                lenString =lineSpace
+                string += "&\n" + lenSpace*' '
+        if string[-2:] == "&\n":
+            string = string[:-4] + ")"
+        else:
+            string = string[:-2] + ")"
+        return string 
+           
 	
 
 class ModelSubroutine(subroutine):
 
-    __slots__=["__model_name","default","__argList"]
-    def __init__(self, pattern=True, model_name=""):
-        self.__model_name = model_name
+    __slots__=["__name","default","__argList","__wrapper", "__subroutineName"]
+    def __init__(self, pattern=True, name="", wrapper="mct", subroutineName="init"):
+        self.__name = name
+        self.__wrapper = wrapper  # identify the wrapper API
+        self.__subroutineName = self.__name + subroutineName+ "_" + self.__wrapper
 	self.default = pattern
 	self.__argList = []		
 
     @property
-    def model_name(self):
-	return self.__model_name
+    def name(self):
+	return self.__name
 
-    @model_name.setter
-    def model_name(self, model_name):
-	if isinstance(model_name, str):
-	    self.__model_name = model_name
+    @name.setter
+    def name(self, name):
+	if isinstance(name, str):
+	    self.__name = model_name
 	else
 	    raise TypeError("model_name must be str type")
 
@@ -46,10 +67,11 @@ class ModelSubroutine(subroutine):
 	else
 	    raise TypeError("_list must be list type")
 	
-			
-
+    
+# this only used for intermediate attrVect? no!!!
 class AttrVect:
-    __slots__ = ['lsize', '__field', '__name', '__nx', '__ny']
+    __slots__ = ['lsize', '__field', '__name', '__nx', '__ny', '__atype', \
+                 '__src', '__dst', '__grid', '__pes', '__manager']
     def __init__(self, lsize=0, field="", name="", nx=0, ny=0, src="", dst="", grid="",pes=""):
         self.lsize = lsize
         self.__field = field
@@ -60,6 +82,10 @@ class AttrVect:
         self.__dst = dst
         self.__grid = grid
         self.__pes = pes
+        if (self.__grid == self.__src) .or. (self.__grid == self.__dst):
+            self.__atype = 0 #"rearr"
+        else:
+            self.__atype = 1 #"smat"
 
     @property
     def field(self):
@@ -69,6 +95,8 @@ class AttrVect:
 	self.__field = fieldValue
     @property
     def name(self):
+        if self.__name = "":
+            self.__name = getManagerName()
 	return self.__name
     @name.setter
     def name(self, nameValue):
@@ -97,15 +125,25 @@ class AttrVect:
     @pes.setter
     def pes(self, pesValue):
         self.__pes = pesValue         
+    @property
+    def atype(self):
+        return self.__atype
+    
+    def bindToManager(self, manager):
+        self.__manager = manager
+
+    def getManageName(self):
+        return self.__manager.AddAttrVect(self)
+        
 
 
 class Model:
-    __slots__ = ['__model_name','__model_init','__model_run','__model_final']
+    __slots__ = ['__model_name','__model_init','__model_run','__model_final', '__manager']
     def __init__(self):
-	self.__model_name=""
-	self.__model_init = model_subroutine() #optional?
-	self.__model_run = model_subroutine()		
-	self.__model_final = model_subroutine()
+	self.__name=""
+	self.__model_init = ModelSubroutine() #optional?
+	self.__model_run = ModelSubroutine()		
+	self.__model_final = ModelSubroutine()
 
     @property
     def model_init(self):
@@ -133,24 +171,55 @@ class Model:
 	
     @property
     def name(self):
-	return self.__model_name
+	return self.__name
 
-    @model_name.setter
+    @name.setter
     def name(self,name)
-	self.__model_name = name
+	self.__name = name
+
+    def bindToManger(self, manager):
+        self.__manager = manager
+
 
 #
 #   Mapper intermediate representation
 #   subroutine ?
 #
 class Mapper:
-    __slots__ = ["__mapType", "__src", "__dst"]
-    def __init__(self, src, dst, mapType="copy"):
+    __slots__ = ["__mapType", "__src", "__dst", "__name"]
+    def __init__(self, src, dst, name="", mapType="copy"):
+        self.__name = name
         self.__mapType = mapType
         self.__src = src
 	self.__dst = dst
+	
+    @property
+    def name(self):
+        return self.__name
 
-    def 
+    @property
+    def src(self):
+        return self.__src
+    @src.setter
+    def src(self, srcValue):
+	if isinstance(srcValue, type(Model)):
+	    self.__src = srcValue
+	else
+	    raise TypeError("src must be Model type")
+  
+    @property
+    def dst(self):
+        return self.__dst
+    @dst.setter
+    def dst(self, dstValue):
+        if isinstance(dstValue, type(Model)):
+            self.__dst = dstValue
+        else
+            raise TypeError("dst must be Model type")
+
+class Gsmap:
+    __slots__=['__gird', '__pes', '__manager']
+    
 
 
 class ir:
