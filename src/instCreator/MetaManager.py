cedf94259d1e50933829a92298a43f64bec8c807
NeoGeon <987378754@qq.com>
2018-11-21 12:45:05 +0800

    now you can create case

---
+++ src/instCreator/MetaManager.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#encoding:utf-8
+#
+#  metaManager manage code location, Macro, and so on
+#
+#
+#
+import sys
+sys.path.append('../parser')
+from instParser import InstParser
+from configParser import ConfigParser
+#from MetaParser import
+class CodeDesc:
+    def __init__(self, name, loc, templateName):
+        self.name = name
+        self.loc = loc
+        self.templateName = templateName
+
+codePathDict = {}
+codePathDict['baseCpl.F90']    = CodeDesc('','','baseCpl_Template.F90')
+codePathDict['baseField.F90']  = CodeDesc('','','baseField_Template.F90')
+codePathDict['deploy_mod.F90'] = CodeDesc('','', 'deploymod_Template.F90')
+codePathDict['global_var.F90'] = CodeDesc('','','globalVar_Template.F90')
+codePathDict['manage.F90']     = CodeDesc('','','procM_Template.F90')
+codePathDict['search_set.py']  = CodeDesc('','','searchSet_Template.py')
+codePathDict['timeCesm.F90']   = CodeDesc('','','timeCesm_Template.F90')
+codePathDict['time_def.F90']   = CodeDesc('','','timeDef_Template.F90')
+
+class MetaManager:
+    codePathDict = codePathDict
+    def __init__(self):
+        self.absPath = ""
+        self.instPath= "" 
+        self.nmlfile = ""
+        self.confPath = ""
+
+    def setConfigMeta(self, confPath):
+        confParser = ConfigParser(confPath)
+        confParser.parse()
+        confParser.writeNml()
+        self.nmlfile = confParser.nmlfile
+
+    def setMacroMeta(self, instSetupPath):
+        instParser = InstParser(instSetupPath)
+        instParser.parse()
+        self.instPath = instParser.instPath
+        self.confPath = instParser.confPath
---
+++ src/parser/instParser.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#encoding:utf-8
+# This module parser the instSetup.xml
+# to create new case
+#
+#
+
+import xml.etree.ElementTree as ET
+from xml.dom.minidom import Document
+import sys
+sys.path.append('../ir')
+#from DataType import *
+
+class InstParser:
+    def __init__(self, xmlFile):
+        self.xmlFile = xmlFile
+        self.confPath = ""
+        self.instPath = ""
+        self.nmlfile = ""
+        self.restartfile = ""
+
+    def parse(self):
+        tree = ET.parse(self.xmlFile)
+        macro = tree.find('macro')
+        self.instPath = macro.find('instPath').text
+        self.confPath = macro.find('confPath').text
+        
+        fileLoc = tree.find('file_location')
+        self.nmlfile = fileLoc.find('nmlfile').text
+        self.restart_file = fileLoc.find('restart_file').text
+       
